a9a02392a3bbdab0a79812676f731b559425c3f7
root <root@centos-s01.alvaware.local>
2016-12-15 17:38:44 +0000

    added exit code to server script

--- serve.sh
+++ serve.sh
@@ -2,4 +2,12 @@
 
 pkill -f jekyll
 
-bundle exec jekyll serve --host 0.0.0.0 --detach
+cd /var/www/html/alperbas.github.io
+
+bundle exec jekyll serve --host 0.0.0.0 --detach > /dev/null
+
+if [ $? == 1 ]; then
+	exit 1
+else
+	exit 0
+fi
